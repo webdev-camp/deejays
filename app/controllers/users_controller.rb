7a28b769dd5db4a6bc16ddd1b9ee36a6d29465cc
Torsten Ruger <torsten@villataika.fi>
2016-01-25 15:30:32 -0800

    edit user
    and related changes from talk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! , :only => :index
+  before_action :check_user , :only => [:show , :edit , :update]
 
   def index
     @q = User.search( params[:q] )
@@ -7,12 +8,33 @@ class UsersController < ApplicationController
     @users = @user_scope.paginate( :page => params[:page], :per_page => 20 )
   end
 
-  def show
+  def check_user
     @user = User.find(params[:id])
     current = current_user
     unless current == @user or current.admin
       return redirect_to :back, :alert => "Access denied."
     end
+  end
+
+  # PATCH/PUT /songs/1
+  # PATCH/PUT /songs/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path , notice: 'Info successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        tag_given
+      end
+    end
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :info, :link,:city , :country)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, :presence => true
+
 end
