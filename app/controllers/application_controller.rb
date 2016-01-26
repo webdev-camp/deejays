61ec42d1aafaf103e2e370d9b71ef9282130940e
Torsten Ruger <torsten@villataika.fi>
2016-01-26 12:36:34 -0800

    fix the fix of registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-      # Fields for sign up
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :city , :country) }
-      # Fields for editing an existing account
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email,:password, :current_password, :city,:country )}
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :songs
   root to: 'visitors#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
   resources :users
 end
