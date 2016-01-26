9227a463101b7b8811b953553f01c5cbe9be2913
Torsten Ruger <torsten@villataika.fi>
2016-01-26 12:00:13 -0800

    temporay diable of validate

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, :presence => true
   validates :artist, :presence => true
-  validates :album, :presence => true
-  validates :speed, :presence => true
-  validates :genre, :presence => true
+#  validates :album, :presence => true
+#  validates :tempo, :presence => true
+#  validates :main_genre, :presence => true
 end
