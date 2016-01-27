c3da9542e43094bcbb6dd8149a682a315720ef37
Torsten Ruger <torsten@villataika.fi>
2016-01-26 20:50:32 -0800

    tighten the belt

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
   validates :title, :presence => true
   validates :artist, :presence => true
-#  validates :album, :presence => true
-#  validates :tempo, :presence => true
-#  validates :main_genre, :presence => true
+  validates :album, :presence => true
+  validates :tempo, :presence => true
+  validates :main_genre, :presence => true
 
   default_scope {order('date_added DESC')}
 
