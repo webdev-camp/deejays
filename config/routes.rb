0524ede07882862adebc61ec693a01c13ffa4f60
Torsten Ruger <torsten@villataika.fi>
2016-01-26 20:05:47 -0800

    add zachs texts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :songs
   root to: 'visitors#index'
-
+  get 'about' => 'visitors#about'
   devise_for :users, :controllers => { registrations: 'registrations' }
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+
   resources :users
 end
