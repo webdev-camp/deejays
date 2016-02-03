68bc091091565a4c84da940136dd31367eeec679
Torsten Ruger <torsten@villataika.fi>
2016-02-02 22:54:40 -0800

    genre scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127043223) do
+ActiveRecord::Schema.define(version: 20160203065114) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
