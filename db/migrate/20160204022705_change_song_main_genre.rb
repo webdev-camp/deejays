024ee35d125f90924304b4b21e9042d147a27c69
Torsten Ruger <torsten@villataika.fi>
2016-02-03 21:09:25 -0800

    fix migration

--- db/migrate/20160204022705_change_song_main_genre.rb
+++ db/migrate/20160204022705_change_song_main_genre.rb
@@ -1,6 +1,6 @@
 class ChangeSongMainGenre < ActiveRecord::Migration
   def change
-    rename_column :songs, :main_genre, :main_genre_id
-    change_column :songs, :main_genre_id, :integer
+    remove_column :songs, :main_genre
+    add_column :songs, :main_genre_id, :integer
   end
 end
