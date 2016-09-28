ead1c7293b62d612c2a544682fd2c7f76eaa163c
Francesca Fagioli <fra.fagiol@gmail.com>
2016-09-28 16:13:44 +0200

    Weird fix.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,4 +1,5 @@
 require 'support/helpers/session_helpers'
+require 'support/helpers/song_helpers'
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::SongHelpers, type: :feature
