f90dab2bcc2808320b9df351faa8d44a821ab728
alexanderspace100 <alexanderspace100@gmail.com>
2016-09-28 16:44:32 +0300

    validates vocals

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,7 @@
 class Genre < ActiveRecord::Base
   validates :name, length: { in: 3..20}
   validates :tempo, numericality: { only_integer: true, :greater_than => 10, :less_than => 200}
+  validates :vocals, :presence => true;
 
   default_scope {order('name ASC')}
 
