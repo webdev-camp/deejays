6e8bae537052e9fed9785004ce565845262d69f9
Francesca Fagioli <fra.fagiol@gmail.com>
2016-09-28 11:23:07 +0200

    Adds validation to Genres.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,8 @@
 class Genre < ActiveRecord::Base
+  validates :name, length: { in: 3..20}
+  validates :tempo, numericality: { only_integer: true, :greater_than => 10, :less_than => 200}
+
   default_scope {order('name ASC')}
+
+
 end
